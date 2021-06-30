19760ab6803ef48013baf6fe963cf8cc4d2bb79e
ShinjeongA <jeonga@khu.ac.kr>
2021-06-30 19:51:09 +0900

    api

---
+++ python_crawling/ch3_open_api.py
@@ -0,0 +1,12 @@
+'''
+#api:application programming interface의 약자, 특정 프로그램을 만들기 위해 제공되는 모듈(함수)을 의미
+#-1.open api: 누구나 사용할 수 있도록 하는 api (REST api 기술을 주로 사용)
+#-2.REST api: http 프로토콜을 통해 서버 제공 기능을 사용할 수 있는 함수를 의미
+        # - 일반적으로 xml, json의 형태로 응답을 전달 (서버와 클라이언트간에 굳이 html 형식만 안 줘도 되니까, xml, json형태로 서버가 클라이언트에게 제공)
+#json이란? 웹 환경에서 서버와 클라이언트 사이에 데이터를 주고 받을 때 많이 사용
++) postman을 통해 rest API를 활용해서 빠르게 결과를(json) 확인할 수 있음
+'''
+
+#네이버 open api
+#client ID: YqapK6EjxIBuZXwS8XoW
+#client server: 35Owa_sX5Q
